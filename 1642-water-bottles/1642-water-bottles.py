149af5beb709d060cb308ba223ad7d3bebce31be
vinaysr93 <8091212+vinaysr93@users.noreply.github.com>
2023-10-20 16:18:24 +0530

    Time: 42 ms (22.19%), Space: 16.3 MB (12.76%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.py
+++ 1642-water-bottles/1642-water-bottles.py
@@ -2,17 +2,11 @@ class Solution:
     def numWaterBottles(self, numBottles, numExchange):
 
         s=numBottles
-        q=numBottles//numExchange
-        r=numBottles%numExchange
-        s=s+q
-        num=q+r
-        while(num>=numExchange):
+        
+        while(numBottles>=numExchange):
             
-            q=num//numExchange
-            print("Q",q)
-            r=num%numExchange
-            print("R",r)
-            s=s+q
-            print("Sum",s)
-            num=q+r
+            r=numBottles%numExchange
+            numBottles=numBottles//numExchange
+            s=s+numBottles
+            numBottles+=r
         return s
