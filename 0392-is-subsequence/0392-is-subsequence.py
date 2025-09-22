98169a71ff209268e6c4ff562384ac9b61c7fd93
vinaysr93 <8091212+vinaysr93@users.noreply.github.com>
2025-09-22 02:58:15 -0700

    Time: 0 ms (100%), Space: 17.8 MB (42.03%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) :
+        count=0
+        t_copy=t
+        for a in s:
+
+            if a in t_copy:
+                r=t_copy.index(a);
+
+                if r>=0:
+                    
+                    t_copy=t_copy[r+1:]
+                    count+=1
+                
+            
+        if count==len(s):
+            return True
+        else:
+            return False
