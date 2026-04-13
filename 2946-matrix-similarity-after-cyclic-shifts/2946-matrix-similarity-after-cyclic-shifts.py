a7cf5dfd4879e7daeaa7bcada2722d2d5773d926
vinaysr93 <8091212+vinaysr93@users.noreply.github.com>
2026-04-12 19:12:34 -0700

    Time: 0 ms (100%), Space: 19.3 MB (85.63%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.py
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.py
@@ -5,54 +5,27 @@ class Solution:
         def areSimilar(self, mat: List[List[int]], k: int) -> bool:
             
                 # Handle left shift and right shift separately
-                mat_or=[]
-
-                for j in mat:
-
-                    lin=[]
-                    for l in j:
-                        lin.append(l)
-                    
-                    mat_or.append(lin)
+               
+                col_len=len(mat[0])
+                k=k%col_len
+                m=len(mat)
+                n=len(mat[0])
 
 
-                def left_shift(arr):
+                for i in range(m):
 
-                    a=arr.pop(0)
-                    arr.append(a)
-
-                    return arr
+                    for j in range(n):
 
 
+                      
 
+                            if mat[i][j]==mat[i][(j+k)%col_len]:
+                                continue
+                            else:
+                                return False
 
-                def right_shift(arr):
-
-                    a=arr.pop()
-                    arr.insert(0,a)
-
-                    return arr
-
-                for x in range (k):
-
-                    for i in range (len (mat)):
-
-                        if i%2==0:
-
-                            rot_arr= left_shift(mat[i])
-
-                            mat[i]=rot_arr
-
-                        else:
-
-                            rot_arr=right_shift(mat[i])
-                            mat[i]=rot_arr
-
-                print(mat)
-                print("Mat_Ori",mat_or)
-                return mat==mat_or
-
-    
+                      
+                return True
 
 
 
