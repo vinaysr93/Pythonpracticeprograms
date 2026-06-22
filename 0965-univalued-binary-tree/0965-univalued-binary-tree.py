ae56fbf54bc9d0bd053e0386330f0bedbcdbe256
vinaysr93 <8091212+vinaysr93@users.noreply.github.com>
2026-06-22 13:19:26 +0530

    Time: 0 ms (100%), Space: 19.4 MB (28.8%) - LeetHub

--- 0965-univalued-binary-tree/0965-univalued-binary-tree.py
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.py
@@ -8,43 +8,20 @@ class Solution:
     def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
         
         if root==None:
-            return False
+            return True
         root_val=root.val
         return(self.helper(root,root_val))
 
     
     def helper(self, root,root_val):
 
-        if root.right!=None and root.left==None:
+       if root==None:
+        return True
+       
+       if root.val!=root_val:
+        return False
+    
 
-            if root.val==root_val:
 
-                return self.helper(root.right,root_val)
-            else:
-                return False
 
-        if root.left!=None and root.right== None:
-
-            if root.val==root_val:
-
-                return self.helper(root.left,root_val)
-            else:
-                return False
-
-        
-        if root.left!=None and root.right!= None:
-            
-            if root.val==root_val:
-                return (self.helper(root.left,root_val) and self.helper(root.right,root_val))
-                #left 1     # right 1
-                #left 1     #returns True
-                #returns true #returns true
-
-            else:
-                return False
-
-        if root.left==None and root.right==None:
-
-            return (root.val==root_val)
-
-            
+       return (self.helper(root.left,root_val) and self.helper(root.right,root.val)) 
